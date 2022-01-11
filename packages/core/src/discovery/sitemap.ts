b99bca4927dd8249ef4ac2b06a619e5533834f0a
Harlan Wilton <harlan@harlanzw.com>
2022-01-11 22:36:36 +1100

    debug: add debugging to sitemap generation

--- packages/core/src/discovery/sitemap.ts
+++ packages/core/src/discovery/sitemap.ts
@@ -1,4 +1,5 @@
 import Sitemapper from 'sitemapper'
+import { useLogger } from '../logger'
 
 /**
  * Fetches routes from a sitemap file.
@@ -6,11 +7,14 @@ import Sitemapper from 'sitemapper'
  * @param site
  */
 export const extractSitemapRoutes = async(site: string) => {
+  const logger = useLogger()
   const sitemap = new Sitemapper({
     timeout: 15000, // 15 seconds
   })
 
   const sitemapUrl = `${site}/sitemap.xml`
+  logger.debug(`Attempting to fetch sitemap at ${sitemapUrl}`)
   const { sites } = await sitemap.fetch(sitemapUrl)
+  logger.debug(`Fetched sitemap with ${sites.length} URLs.`)
   return sites
 }
