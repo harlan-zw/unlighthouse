460fa112ab74b56cf4407cffcec2f217b7091ddb
Harlan Wilton <harlan@harlanzw.com>
2022-01-13 23:10:33 +1100

    fix(tests): debug logs

--- test/ci.test.ts
+++ test/ci.test.ts
@@ -7,41 +7,42 @@ export const cacheDir = resolve(__dirname, '.cache')
 export const ci = resolve(__dirname, '../packages/unlighthouse/bin/unlighthouse-ci.cjs')
 
 beforeAll(async() => {
-    await fs.remove(cacheDir)
+  await fs.remove(cacheDir)
 })
 
 afterAll(async() => {
-    await fs.remove(cacheDir)
+  await fs.remove(cacheDir)
 })
 
 describe('ci', () => {
-    it('tests harlanzw.com', async() => {
-        const { output } = await runCli(resolve(__dirname, 'fixtures/harlanzw.config.ts'))
+  it('tests harlanzw.com', async() => {
+    const { output } = await runCli(resolve(__dirname, 'fixtures/harlanzw.config.ts'))
 
-        expect(output).toMatchSnapshot()
-    })
+    expect(output).toMatchSnapshot()
+  })
 })
 
 async function runCli(configFileFixture: string) {
-    const testDir = resolve(cacheDir, Date.now().toString())
+  const testDir = resolve(cacheDir, Date.now().toString())
 
-    await fs.ensureDir(testDir)
+  await fs.ensureDir(testDir)
 
-    const config = await fs.readFile(configFileFixture, 'utf8')
-    await fs.writeFile(join(testDir, 'unlighthouse.config.ts'), config)
+  const config = await fs.readFile(configFileFixture, 'utf8')
+  await fs.writeFile(join(testDir, 'unlighthouse.config.ts'), config)
 
-    const { exitCode, stdout, stderr } = await execa('node', [ci, '--root', testDir, '--debug', '--host', 'unlighthouse.dev'], {
-        cwd: testDir,
-    })
+  const { exitCode, stdout, stderr } = await execa('node', [ci, '--root', testDir, '--debug', '--host', 'harlanzw.com'], {
+    cwd: testDir,
+  })
 
-    const logs = stdout + stderr
-    if (exitCode !== 0)
-        throw new Error(logs)
+  const logs = stdout + stderr
+  console.log(logs)
+  if (exitCode !== 0)
+    throw new Error(logs)
 
-    const output = await fs.readJson(resolve(testDir, '.lighthouse', 'ci-result.json'))
+  const output = await fs.readJson(resolve(testDir, '.lighthouse', 'ci-result.json'))
 
-    return {
-        output,
-        logs,
-    }
+  return {
+    output,
+    logs,
+  }
 }
