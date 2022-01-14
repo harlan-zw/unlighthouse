b4e15bdb1381a9e714cebebb398fc0ebe4c93c5b
Harlan Wilton <harlan@harlanzw.com>
2022-01-14 19:30:44 +1100

    feat(doc): add panelbear analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,21 @@
 import 'windi.css'
 import DefaultTheme from 'vitepress/theme'
 import '../../main.css'
+import * as Panelbear from '@panelbear/panelbear-js'
+
+const theme = DefaultTheme
+
+theme.enhanceApp = ({ app }) => {
+    // if we're in a server context then we exit out here
+    if (typeof document === 'undefined' || typeof window === 'undefined') {
+        return
+    }
+    app.provide('analytics', Panelbear)
+    Panelbear.load('JHwFJG78euO', {
+        spaMode: 'history',
+        autoTrack: true,
+        debug: import.meta.env.DEV
+    })
+}
 
 export default DefaultTheme
