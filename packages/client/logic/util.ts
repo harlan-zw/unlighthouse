573ce12df7fa8a06601cf3d157d45de60a4707f0
Harlan Wilton <harlan@harlanzw.com>
2022-01-17 14:56:30 +1100

    fix(client): format sizes, prefer B over Bytes

--- packages/client/logic/util.ts
+++ packages/client/logic/util.ts
@@ -19,11 +19,11 @@ export const extractFgColor = (str: string) => {
 }
 
 export const formatBytes = (bytes: number, decimals = 2) => {
-  if (bytes === 0) return '0 Bytes'
+  if (bytes === 0) return '0B'
 
   const k = 1024
   const dm = decimals < 0 ? 0 : decimals
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
   const i = Math.floor(Math.log(bytes) / Math.log(k))
 
