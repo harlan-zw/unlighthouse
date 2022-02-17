bd958250d105093f59bf2827352df4f6bacf8404
Harlan Wilton <harlan@harlanzw.com>
2022-02-17 16:45:13 +1100

    feat(client): dark mode by default, ignore system preference

--- packages/client/logic/dark.ts
+++ packages/client/logic/dark.ts
@@ -1,4 +1,26 @@
-import { useDark, useToggle } from '@vueuse/core'
+import { useToggle, useStorage } from '@vueuse/core'
 
-export const isDark = useDark()
+export const mode = useStorage('vueuse-color-scheme', 'dark')
+export const isDark = computed<boolean>({
+  get() {
+    return mode.value === 'dark'
+  },
+  set(v) {
+    mode.value = v ? 'dark' : 'light'
+  },
+})
+
+watch(isDark, () => {
+  const el = window?.document.querySelector('html')
+  if (!el)
+    return
+  if (isDark.value) {
+    el.classList.add('dark')
+    el.classList.remove('light')
+  } else {
+    el.classList.add('light')
+    el.classList.remove('dark')
+  }
+}, { flush: 'post', immediate: true })
+
 export const toggleDark = useToggle(isDark)
