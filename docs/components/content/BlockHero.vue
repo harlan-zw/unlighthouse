5f4f11d0eec8bae689af9faf10743d24cc09c42f
Harlan Wilton <harlan@harlanzw.com>
2023-05-11 11:59:28 +0900

    chore: lint

--- docs/components/content/BlockHero.vue
+++ docs/components/content/BlockHero.vue
@@ -22,48 +22,48 @@ defineProps({
 </script>
 
 <template>
-<section class="py-5 sm:py-10 xl:py-20">
-  <div class="xl:grid gap-8 lg:grid-cols-12 mx-auto w-full sm:px-6 lg:px-0 px-0">
-    <div class="col-span-6 mb-10 lg:mb-0 flex flex-col justify-center">
-      <p v-if="$slots.top" class="mb-2 text-center lg:text-left">
-        <ContentSlot :use="$slots.top" unwrap="p" />
-      </p>
+  <section class="py-5 sm:py-10 xl:py-20">
+    <div class="xl:grid gap-8 lg:grid-cols-12 mx-auto w-full sm:px-6 lg:px-0 px-0">
+      <div class="col-span-6 mb-10 lg:mb-0 flex flex-col justify-center">
+        <p v-if="$slots.top" class="mb-2 text-center lg:text-left">
+          <ContentSlot :use="$slots.top" unwrap="p" />
+        </p>
 
-      <h1 class="text-gray-900 dark:text-gray-100 text-center text-4xl leading-25 font-extrabold tracking-tight sm:text-5xl lg:text-left lg:text-6xl" style="line-height: 1.3;">
-        <ContentSlot :use="$slots.title" unwrap="p" />
-      </h1>
+        <h1 class="text-gray-900 dark:text-gray-100 text-center text-4xl leading-25 font-extrabold tracking-tight sm:text-5xl lg:text-left lg:text-6xl" style="line-height: 1.3;">
+          <ContentSlot :use="$slots.title" unwrap="p" />
+        </h1>
 
-      <p class="text-gray-700 dark:text-gray-300 mt-4 max-w-3xl text-center text-lg lg:text-left">
-        <ContentSlot :use="$slots.description" unwrap="p" />
-      </p>
+        <p class="text-gray-700 dark:text-gray-300 mt-4 max-w-3xl text-center text-lg lg:text-left">
+          <ContentSlot :use="$slots.description" unwrap="p" />
+        </p>
 
-      <div v-if="$slots.extra" class="mt-6">
-        <ContentSlot :use="$slots.extra" unwrap="p" />
+        <div v-if="$slots.extra" class="mt-6">
+          <ContentSlot :use="$slots.extra" unwrap="p" />
+        </div>
+
+        <div class="mt-6 flex flex-col items-center justify-center gap-4 sm:mt-10 sm:flex-row sm:gap-6 lg:justify-start">
+          <template v-if="!$slots.actions">
+            <ButtonLink v-if="cta" class="!mb-0 dark:bg-green-800!" bold size="large" :href="cta[1] as any">
+              {{ cta[0] }}
+            </ButtonLink>
+
+            <a v-if="secondary" :href="secondary[1] as any" class="u-text-gray-500 hover:u-text-gray-700 py-px font-medium">
+              {{ secondary[0] }}
+            </a>
+          </template>
+          <ContentSlot v-else :use="$slots.actions" unwrap="p" />
+        </div>
       </div>
 
-      <div class="mt-6 flex flex-col items-center justify-center gap-4 sm:mt-10 sm:flex-row sm:gap-6 lg:justify-start">
-        <template v-if="!$slots.actions">
-        <ButtonLink v-if="cta" class="!mb-0 dark:bg-green-800!" bold size="large" :href="cta[1] as any">
-          {{ cta[0] }}
-        </ButtonLink>
-
-        <a v-if="secondary" :href="secondary[1] as any" class="u-text-gray-500 hover:u-text-gray-700 py-px font-medium">
-          {{ secondary[0] }}
-        </a>
-        </template>
-        <ContentSlot v-else :use="$slots.actions" unwrap="p" />
+      <div class="block-hero__right xl:col-span-6 max-w-full">
+        <div class="flex relative items-center block-hero__inner xl:p-10 bg-gradient-to-br to-green-200 from-blue-100 dark:from-green-500/10 dark:to-blue-500/20 rounded">
+          <slot name="right">
+            <ContentSlot :use="$slots.right" unwrap="p" />
+          </slot>
+        </div>
       </div>
     </div>
-
-    <div class="block-hero__right xl:col-span-6 max-w-full">
-      <div class="flex relative items-center block-hero__inner xl:p-10 bg-gradient-to-br to-green-200 from-blue-100 dark:from-green-500/10 dark:to-blue-500/20 rounded">
-        <slot name="right">
-          <ContentSlot :use="$slots.right" unwrap="p" />
-        </slot>
-      </div>
-    </div>
-  </div>
-</section>
+  </section>
 </template>
 
 <style scoped>
