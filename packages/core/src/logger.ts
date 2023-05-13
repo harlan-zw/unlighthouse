9ef7aba40bdb48b21dd20af85922c49230f86773
Harlan Wilton <harlan@harlanzw.com>
2023-05-13 13:50:15 +0900

    chore: bump deps

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,28 +1,29 @@
-import type { Consola } from 'consola'
-import consola from 'consola'
+import type { ConsolaInstance } from 'consola'
+import { createConsola } from 'consola'
 import { createContext } from 'unctx'
 import { AppName } from './constants'
 
-const loggerCtx = createContext<Consola>()
-/**
- * Gets the instantiated logger instance using the shared context, persists the application logging configuration.
- */
-export const useLogger: () => Consola = () => {
-  let logger = loggerCtx.use()
-  // just in-case the logger wasn't initialised, we want to always return an instance to avoid null checks in DX
-  if (!logger)
-    logger = consola.withScope(AppName)
-
-  return logger
-}
+const loggerCtx = createContext<ConsolaInstance>()
 
 export const createLogger = (debug = false) => {
-  const logger = consola.withScope(AppName)
+  const logger = createConsola().withTag(AppName)
 
   if (debug) {
     // debug
     logger.level = 4
   }
   loggerCtx.set(logger)
+  return logger
+}
+
+/**
+ * Gets the instantiated logger instance using the shared context, persists the application logging configuration.
+ */
+export const useLogger: () => ConsolaInstance = () => {
+  let logger = loggerCtx.use()
+  // just in-case the logger wasn't initialised, we want to always return an instance to avoid null checks in DX
+  if (!logger)
+    logger = createLogger()
+
   return logger
 }
