c9620507d22f45aba1116fceae1edd7654339f6c
Tomas Jansson <tomas.jansson@gmail.com>
2023-05-19 23:35:40 +0200

    Full report for ci v1Report

--- test/fixtures/harlanzw-v1report.config.ts
+++ test/fixtures/harlanzw-v1report.config.ts
@@ -1,21 +1,20 @@
 export default {
-  site: 'https://harlanzw.com',
+  site: "https://harlanzw.com",
   cache: false,
   scanner: {
-    device: 'desktop',
+    device: "desktop",
     throttle: false,
   },
   ci: {
     budget: {
-      'best-practices': 80,
-      'seo': 85,
-      'accessibility': 85,
+      "best-practices": 80,
+      seo: 85,
+      accessibility: 85,
     },
     v1Report: true,
   },
-  lighthouseOptions: {
-    onlyCategories: ['best-practices', 'seo', 'accessibility']
-  },
+  // lighthouseOptions: {
+  //   onlyCategories: ['best-practices', 'seo', 'accessibility']
+  // },
   debug: true,
-}
-
+};
