31a301370c7cf5813e2824466131c4e8cfe90635
harlan <harlan@harlanzw.com>
2024-03-01 10:49:12 +1100

    fix(client): make report link more obvious

--- packages/client/components/Cell/CellScoresOverview.vue
+++ packages/client/components/Cell/CellScoresOverview.vue
@@ -21,8 +21,9 @@ defineProps<{
         <metric-guage :score="val.score" :label="val.title" />
       </div>
     </div>
-    <div class="text-xs opacity-60 mt-1 text-center" style="font-size: 10px;">
-      View Report
+    <div class="text-xs opacity-80 mt-2 text-left flex items-center gap-1">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32" class="w-4 h-4"><path fill="currentColor" d="M15 20h2v4h-2zm5-2h2v6h-2zm-10-4h2v10h-2z"/><path fill="currentColor" d="M25 5h-3V4a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v1H7a2 2 0 0 0-2 2v21a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2M12 4h8v4h-8Zm13 24H7V7h3v3h12V7h3Z"/></svg>
+      <span>View Lighthouse Report</span>
     </div>
   </btn-action>
 </template>
