7b9803de26ea44e9be725ba3ccba17725bc75576
edulamgle001 <Gleni.Lame@edu.fh-kaernten.ac.at>
2024-08-13 22:23:21 +0200

    json i ndrrum, build command i ndrru2m

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,7 +1,18 @@
 export default {
-    site: 'https://allwaysmoving.com/',
-    debug: true,
-    scanner: {
-      device: 'desktop'
-    }
-  }
+  site: 'harlanzw.com',
+  scanner: {
+    // exclude specific routes
+    exclude: [
+      '/.*?pdf',
+      '.*/amp',
+      'en-*',
+    ],
+    // run lighthouse for each URL 3 times
+    samples: 3,
+    // use desktop to scan
+    device: 'desktop',
+    // enable the throttling mode
+    throttle: true,
+  },
+  debug: true,
+}
