008cbefa8f74e79121fc4b46e67315c29d1abea0
Harlan Wilton <harlan@harlanzw.com>
2026-04-16 02:24:02 +1000

    fix(client): repair web UI broken by HeadlessUI removal (#305)

--- packages/client/components/Tooltip.vue
+++ packages/client/components/Tooltip.vue
@@ -4,23 +4,21 @@ const isShowing = ref(false)
 
 <template>
   <div v-if="$slots.tooltip" class="relative" @mouseleave="isShowing = false">
-    <TransitionRoot
+    <Transition
       appear
-      :show="isShowing"
-      as="template"
-      enter="transform transition duration-400"
-      enter-from="opacity-0 scale-50"
-      enter-to="opacity-100 scale-100"
-      leave="transform duration-200 transition ease-in-out"
-      leave-from="opacity-100 scale-100 "
-      leave-to="opacity-0 scale-95 "
+      enter-active-class="transform transition duration-400"
+      enter-from-class="opacity-0 scale-50"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="transform duration-200 transition ease-in-out"
+      leave-from-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
     >
-      <div role="tooltip" class="max-w-[500px] z-500 flex -left-[80px] absolute w-auto min-w-[320px] max-h-[500px] overflow-y-auto z-10 top-[99%] inline-block bg-gray-900/99 font-medium shadow-xs text-white py-2 px-3 text-sm rounded-lg">
+      <div v-if="isShowing" role="tooltip" class="max-w-[500px] z-500 flex -left-[80px] absolute w-auto min-w-[320px] max-h-[500px] overflow-y-auto z-10 top-[99%] inline-block bg-gray-900/99 font-medium shadow-xs text-white py-2 px-3 text-sm rounded-lg">
         <div>
           <slot name="tooltip" />
         </div>
       </div>
-    </TransitionRoot>
+    </Transition>
     <span @mouseenter="isShowing = true">
       <slot />
     </span>
