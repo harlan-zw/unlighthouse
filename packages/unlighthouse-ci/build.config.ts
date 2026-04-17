b2bee2b6dd00f0c5c001a4423135d82eb8edf02e
Harlan Wilton <harlan@harlanzw.com>
2026-04-17 11:20:10 +1000

    chore: port unlighthouse-ci package from chore/v1

---
+++ packages/unlighthouse-ci/build.config.ts
@@ -0,0 +1,10 @@
+import { defineBuildConfig } from 'obuild/config'
+
+export default defineBuildConfig({
+  entries: [
+    {
+      type: 'bundle',
+      input: ['./src/index.ts'],
+    },
+  ],
+})
